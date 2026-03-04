07fb1f05644395c7908e7a14c25b26bdfbdda3ce
qodiraliabbos95-alt <qodiraliabbos95@gmail.com>
2026-03-04 17:51:15 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,24 @@ import streamlit as st
 from main import mega_math_engine
 
 st.set_page_config(page_title="MEGA MATH AI")
-st.title("🧮 MEGA MATH AI")
 
-command = st.text_input("Masalani kiriting:", "solve 2*x+5=15")
+st.title("🧮 MEGA MATH AI")
+st.write("Sun'iy intellekt matematik yechuvchi")
+
+command = st.text_input("Masalani kiriting:")
 
 if st.button("Yechish"):
-    result = mega_math_engine(command)
-    st.write("### Natija:")
-    if isinstance(result, list):
-        for step in result:
-            st.write(step)
-    else:
-        st.write(result)
+    if command:
+        result = mega_math_engine(command)
 
-st.write("**Misol:** solve 2*x+5=15")
+        st.write("### Natija:")
+
+        if isinstance(result, list):
+            for step in result:
+                st.write(step)
+        else:
+            st.write(result)
+    else:
+        st.warning("Masala kiriting!")
+
+st.write("Misol: solve 2*x+5=15")
